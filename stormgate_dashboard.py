6ad85f81b29ba4b3da47a2073f8044885c8440dd
kiim11 <kiim11@yandex.ru>
2025-08-23 12:47:01 +0300

    Update stormgate_dashboard.py

--- stormgate_dashboard.py
+++ stormgate_dashboard.py
@@ -52,9 +52,9 @@ st.markdown("""
 if 'filters' not in st.session_state:
     # Try to load filters from local storage
     saved_filters = local_storage.getItem("stormgate_filters")
-    if saved_filters and saved_filters.value:
+    if saved_filters:
         try:
-            st.session_state.filters = json.loads(saved_filters.value)
+            st.session_state.filters = json.loads(saved_filters)
             st.sidebar.markdown('<div class="storage-notification">Loaded saved filters from storage!</div>', unsafe_allow_html=True)
         except:
             st.session_state.filters = {
